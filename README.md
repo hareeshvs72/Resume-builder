6779d4f02144a6a8838280d8a4da6c95eb5c85d7
hareeshvs72 <hareeshvs72@gmail.com>
2025-08-11 10:56:33 +0530

    resume builder day 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Resume Builde Creation Steps
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage'
+import History from './pages/History'
+import PageNotFound from './pages/PageNotFound'
+import ResumeGenerator from './pages/ResumeGenerator'
+import UserForm from './pages/UserForm'
+ 
+function App() {
+  
+
+  return (
+    <>
+       <Header/>
+         <Routes>
+            <Route path='/' element={<LandingPage/>} />
+            <Route path='/History' element={<History/>} />
+            <Route path='/ResumeGenerator' element={<ResumeGenerator/>} />
+            <Route path='/UserForm' element={<UserForm/>} />
+            <Route path='/*' element={<PageNotFound/>} />
+         </Routes>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
